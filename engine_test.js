a88c9988734136e8444f93f029d723335e492b74
Daniel Boczek <daniel.boczek@codepill.com>
2011-06-09 13:59:26 +0200

    Changed tests for crafty so they are identical with tests for cocos2d

--- engine_test.js
+++ engine_test.js
@@ -1,4 +1,4 @@
-module("Viewport", {
+q.module("Viewport", {
     setup: function(){
         engine.initViewport(1000, 700); // in pixels
     },
@@ -7,7 +7,7 @@ module("Viewport", {
     }
 });
 
-asyncTest('Move viewport', 1, function() {
+q.asyncTest('Move viewport', 1, function() {
     engine.prepareWorldData(48,48, function(){
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -17,15 +17,14 @@ asyncTest('Move viewport', 1, function() {
             engine.moveViewportTo(600, 600, 3000, function(){
                 var stopTime = new Date();
                 var delta = stopTime.getTime() - startTime.getTime();
-                ok(delta < 3020 && delta > 2980, "Animation was performed in resonable time 2980 < " + delta + " < 3020 ");
-                start();
+                q.ok(delta < 3020 && delta > 2980, "Animation was performed in resonable time 2980 < " + delta + " < 3020 ");
+                q.start();
             });
         }, 500);
-        
     });
 });
 
-asyncTest('Switch scene', 1, function() {
+q.asyncTest('Switch scene', 1, function() {
     engine.prepareWorldData(48,48, function(){
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -34,14 +33,14 @@ asyncTest('Switch scene', 1, function() {
             var startTime = new Date();
             engine.showScene('scene2', function(){
                 var stopTime = new Date();
-                ok((stopTime.getTime() - startTime.getTime()) < 1000, 'Render time: ' + (stopTime.getTime() - startTime.getTime()));
-                start();
+                q.ok((stopTime.getTime() - startTime.getTime()) < 1000, 'Render time: ' + (stopTime.getTime() - startTime.getTime()));
+                q.start();
             });
         }, 500);
     });
 });
 
-asyncTest('Moving non-animated sprite on map', 1, function() {
+q.asyncTest('Moving non-animated sprite on map', 1, function() {
     engine.prepareWorldData(48,48, function(){
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -52,14 +51,14 @@ asyncTest('Moving non-animated sprite on map', 1, function() {
             entity.moveTo(15, 10, 10000, function() {
                 var stopTime = new Date();
                 var delta = (stopTime.getTime() - startTime.getTime());
-                ok( 9900 < delta && delta < 10100, "Sprite moved in about 10 seconds (" + delta + ")");
-                start();
+                q.ok( 9900 < delta && delta < 10100, "Sprite moved in about 10 seconds (" + delta + ")");
+                q.start();
             });
         }, 500);
     });
 });
 
-asyncTest('Moving 50 non-animated sprites on map', 50, function() {
+q.asyncTest('Moving 50 non-animated sprites on map', 50, function() {
     engine.prepareWorldData(48,48, function(){
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -81,16 +80,16 @@ asyncTest('Moving 50 non-animated sprites on map', 50, function() {
                     entity.moveTo(entities[n].finish.x, entities[n].finish.y, 5000, function() {
                         var stopTime = new Date();
                         var delta = (stopTime.getTime() - startTime.getTime());
-                        ok( 4900 < delta && delta < 5100, "Sprite moved in about 5 seconds (" + delta + ")");
+                        q.ok( 4900 < delta && delta < 5100, "Sprite moved in about 5 seconds (" + delta + ")");
                     });
                 })();
             }
-            setTimeout(function(){start();}, 6000);
+            setTimeout(function(){q.start();}, 6000);
         }, 1000);
     });
 });
 
-asyncTest('Moving aminated sprite on map', function(){
+q.asyncTest('Moving aminated sprite on map', function(){
     engine.prepareWorldData(48, 48, function() {
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -100,12 +99,12 @@ asyncTest('Moving aminated sprite on map', function(){
             startTime = new Date();
             entity.playAnimation();
             entity.moveTo(1, 8, 5000, function() {
-                entity.rotate(-90, 1000, function() {
+                entity.rotate(-90, 1000, 24, 24, function() {
                     entity.moveTo(8,8, 5000, function() {
                         var stopTime = new Date();
                         var delta = (stopTime.getTime() - startTime.getTime());
-                        ok( 10900 < delta && delta < 11100, "Animated sprite moved in about 11 seconds (" + delta + ")");
-                        start();
+                        q.ok( 10900 < delta && delta < 11100, "Animated sprite moved in about 11 seconds (" + delta + ")");
+                        q.start();
                     })
                 });
             });
@@ -113,7 +112,7 @@ asyncTest('Moving aminated sprite on map', function(){
     });
 });
 
-asyncTest('Moving 50 aminated sprites on map', 50, function() {
+q.asyncTest('Moving 50 aminated sprites on map', 50, function() {
     engine.prepareWorldData(48,48, function(){
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -136,16 +135,16 @@ asyncTest('Moving 50 aminated sprites on map', 50, function() {
                     entity.moveTo(entities[n].finish.x, entities[n].finish.y, 5000, function() {
                         var stopTime = new Date();
                         var delta = (stopTime.getTime() - startTime.getTime());
-                        ok( 4900 < delta && delta < 5100, "Animated sprite moved in about 5 seconds (" + delta + ")");
+                        q.ok( 4900 < delta && delta < 5100, "Animated sprite moved in about 5 seconds (" + delta + ")");
                     });
                 })();
             }
-            setTimeout(function(){start();}, 6000);
+            setTimeout(function(){q.start();}, 6000);
         }, 1000);
     });
 });
 
-asyncTest('Moving 50 aminated sprites on animated map', 50, function() {
+q.asyncTest('Moving 50 aminated sprites on animated map', 50, function() {
     engine.prepareWaterWorldData(48,48, function(){
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -168,16 +167,16 @@ asyncTest('Moving 50 aminated sprites on animated map', 50, function() {
                     entity.moveTo(entities[n].finish.x, entities[n].finish.y, 5000, function() {
                         var stopTime = new Date();
                         var delta = (stopTime.getTime() - startTime.getTime());
-                        ok( 4900 < delta && delta < 5100, "Animated sprite moved in about 5 seconds (" + delta + ")");
+                        q.ok( 4900 < delta && delta < 5100, "Animated sprite moved in about 5 seconds (" + delta + ")");
                     });
                 })();
             }
-            setTimeout(function(){start();}, 6000);
+            setTimeout(function(){q.start();}, 6000);
         }, 1000);
     });
 });
 
-asyncTest('Moving 50 aminated sprites on animated background', 50, function() {
+q.asyncTest('Moving 50 aminated sprites on animated background', 50, function() {
     engine.prepareWaterBckWorldData(48,48, function(){
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -200,16 +199,16 @@ asyncTest('Moving 50 aminated sprites on animated background', 50, function() {
                     entity.moveTo(entities[n].finish.x, entities[n].finish.y, 5000, function() {
                         var stopTime = new Date();
                         var delta = (stopTime.getTime() - startTime.getTime());
-                        ok( 4900 < delta && delta < 5100, "Animated sprite moved in about 5 seconds (" + delta + ")");
+                        q.ok( 4900 < delta && delta < 5100, "Animated sprite moved in about 5 seconds (" + delta + ")");
                     });
                 })();
             }
-            setTimeout(function(){start();}, 6000);
+            setTimeout(function(){q.start();}, 6000);
         }, 1000);
     });
 });
 
-asyncTest('Moving 50 aminated sprites on animated background and static map', 50, function() {
+q.asyncTest('Moving 50 aminated sprites on animated background and static map', 50, function() {
     engine.prepareMixedWorldData(48,48, function() {
         engine.createScene('scene1');
         engine.showScene('scene1');
@@ -232,57 +231,58 @@ asyncTest('Moving 50 aminated sprites on animated background and static map', 50
                     entity.moveTo(entities[n].finish.x, entities[n].finish.y, 5000, function() {
                         var stopTime = new Date();
                         var delta = (stopTime.getTime() - startTime.getTime());
-                        ok( 4900 < delta && delta < 5100, "Animated sprite moved in about 5 seconds (" + delta + ")");
+                        q.ok( 4900 < delta && delta < 5100, "Animated sprite moved in about 5 seconds (" + delta + ")");
                     });
                 })();
             }
-            setTimeout(function(){start();}, 6000);
+            setTimeout(function(){q.start();}, 6000);
         }, 1000);
     });
 });
 
-asyncTest('Move 2 large non-animated objects', 4, function() {
+q.asyncTest('Move 2 large non-animated objects', 4, function() {
     engine.prepareWorldData(48,48, function() {
-        engine.createScene('scene1');
-        engine.showScene('scene1');
-        setTimeout(function() {
-            var clouds1 = engine.putEntity(0, 0, 'cloudshigh'),
-                clouds2 = engine.putEntity(10, 10, 'cloudslow'),
-                startTime = new Date();
-            clouds1.setZ(1);
-            clouds2.setZ(2);
-            clouds1.moveTo(10, 10, 5000, function() {
-                var stopTime = new Date();
-                var delta = (stopTime.getTime() - startTime.getTime());
-                ok( 4900 < delta && delta < 5100, "Gray large object moved in about 5 seconds (" + delta + ")");
-            });
-            clouds2.moveTo(0, 0, 5000, function() {
-                var stopTime = new Date();
-                var delta = (stopTime.getTime() - startTime.getTime());
-                ok( 4900 < delta && delta < 5100, "Blue large object moved in about 5 seconds (" + delta + ")");
-            });
-            setTimeout(function() {
-                clouds1.setZ(2);
-                clouds2.setZ(1);
-                startTime = new Date();
-                clouds1.moveTo(0, 0, 5000, function() {
-                    var stopTime = new Date();
-                    var delta = (stopTime.getTime() - startTime.getTime());
-                    ok( 4900 < delta && delta < 5100, "Gray large object moved in about 5 seconds (" + delta + ")");
-                });
-                clouds2.moveTo(10, 10, 5000, function() {
-                    var stopTime = new Date();
-                    var delta = (stopTime.getTime() - startTime.getTime());
-                    ok( 4900 < delta && delta < 5100, "Blue large object moved in about 5 seconds (" + delta + ")");
-                });
-                setTimeout(function() {
-                    start();
-                }, 5500);
-            }, 5500);
-        }, 1000);
-    });
+       engine.createScene('scene1');
+       engine.showScene('scene1');
+       setTimeout(function() {
+           var clouds1 = engine.putEntity(0, 0, 'cloudshigh'),
+               clouds2 = engine.putEntity(10, 10, 'cloudslow'),
+               startTime = new Date();
+           clouds1.setZ(1);
+           clouds2.setZ(2);
+           clouds1.moveTo(10, 10, 5000, function() {
+               var stopTime = new Date();
+               var delta = (stopTime.getTime() - startTime.getTime());
+               q.ok( 4900 < delta && delta < 5100, "Gray large object moved in about 5 seconds (" + delta + ")");
+           });
+           clouds2.moveTo(0, 0, 5000, function() {
+               var stopTime = new Date();
+               var delta = (stopTime.getTime() - startTime.getTime());
+               q.ok( 4900 < delta && delta < 5100, "Blue large object moved in about 5 seconds (" + delta + ")");
+           });
+           setTimeout(function() {
+               clouds1.setZ(2);
+               clouds2.setZ(1);
+               startTime = new Date();
+               clouds1.moveTo(0, 0, 5000, function() {
+                   var stopTime = new Date();
+                   var delta = (stopTime.getTime() - startTime.getTime());
+                   q.ok( 4900 < delta && delta < 5100, "Gray large object moved in about 5 seconds (" + delta + ")");
+               });
+               clouds2.moveTo(10, 10, 5000, function() {
+                   var stopTime = new Date();
+                   var delta = (stopTime.getTime() - startTime.getTime());
+                   q.ok( 4900 < delta && delta < 5100, "Blue large object moved in about 5 seconds (" + delta + ")");
+               });
+               setTimeout(function() {
+                   q.start();
+               }, 5500);
+           }, 5500);
+       }, 1000);
+   });
 });
-asyncTest('Rotate 50 small objects over map', 50, function() {
+
+q.asyncTest('Rotate 50 small objects over map', 50, function() {
     engine.prepareWorldData(48, 48, function() {
         engine.createScene('sc1');
         engine.showScene('sc1');
@@ -310,15 +310,15 @@ asyncTest('Rotate 50 small objects over map', 50, function() {
                                   function() {
                         var stopTime = new Date(),
                             delta = stopTime.getTime() - startTime.getTime();
-                        ok(4900 < delta && delta < 5100, 'Small object rotated in about 5 seconds (' + delta + ')');
+                        q.ok(4900 < delta && delta < 5100, 'Small object rotated in about 5 seconds (' + delta + ')');
                     });
-                    setTimeout(function() {start();}, 6000);
+                    setTimeout(function() {q.start();}, 6000);
                 })();
             }
         }, 1000);
     })
 });
-asyncTest('Rotate 2 large objects over map', 2, function() {
+q.asyncTest('Rotate 2 large objects over map', 2, function() {
     engine.prepareWorldData(48, 48, function() {
         engine.createScene('sc1');
         engine.showScene('sc1');
@@ -329,18 +329,18 @@ asyncTest('Rotate 2 large objects over map', 2, function() {
             cloud1.rotate(360, 5000, 240, 120, function() {
                 var stopTime = new Date(),
                     delta = stopTime.getTime() - startTime.getTime();
-                ok(4900 < delta && delta < 5100, 'Large object rotated in about 5 seconds (' + delta + ')');
+                q.ok(4900 < delta && delta < 5100, 'Large object rotated in about 5 seconds (' + delta + ')');
             });
             cloud2.rotate(360, 5000, 240, 120, function() {
                 var stopTime = new Date(),
                     delta = stopTime.getTime() - startTime.getTime();
-                ok(4900 < delta && delta < 5100, 'Large object rotated in about 5 seconds (' + delta + ')');
+                q.ok(4900 < delta && delta < 5100, 'Large object rotated in about 5 seconds (' + delta + ')');
             });
-            setTimeout(function() {start();}, 6000);
+            setTimeout(function() {q.start();}, 6000);
         }, 1000);
     })
 });
-asyncTest('Show texts', 3, function() {
+q.asyncTest('Show texts', 3, function() {
     var theFont = 'Arial',
         fontSize = '12px',
         textLeft = {
@@ -365,10 +365,10 @@ asyncTest('Show texts', 3, function() {
         engine.putText(textCenter);
         engine.putText(textRight);
         setTimeout(function() {
-            ok($('*:contains("' + textLeft.text + '"):last').text() === textLeft.text, 'Text aligned left found');
-            ok($('*:contains("' + textCenter.text + '"):last').text() === textCenter.text, 'Centered text found');
-            ok($('*:contains("' + textRight.text + '"):last').text() === textRight.text, 'Text aligned right found');
-            start();
+            q.ok($('*:contains("' + textLeft.text + '"):last').text() === textLeft.text, 'Text aligned left found');
+            q.ok($('*:contains("' + textCenter.text + '"):last').text() === textCenter.text, 'Centered text found');
+            q.ok($('*:contains("' + textRight.text + '"):last').text() === textRight.text, 'Text aligned right found');
+            q.start();
         }, 500);
     }, 1000);
 });
